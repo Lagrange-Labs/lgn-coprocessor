498ee5f14ce8b1b76d9749e77b4857cde8bb3f35
Augusto F. Hack <hack.augusto@gmail.com>
2024-07-17 15:06:42 +0200

    chore: rename router -> manager

--- lgn-worker/src/main.rs
+++ lgn-worker/src/main.rs
@@ -244,28 +244,28 @@ fn run(config: &Config) -> Result<()> {
     }
 }
 
-fn register_provers(config: &Config, router: &mut ProversManager) {
+fn register_provers(config: &Config, manager: &mut ProversManager) {
     if config.worker.instance_type >= WorkerClass::Small {
         info!("Creating query prover");
-        register_v0_ecr721_query_prover(config, router);
-        register_v0_ecr20_query_prover(config, router);
+        register_v0_ecr721_query_prover(config, manager);
+        register_v0_ecr20_query_prover(config, manager);
         info!("Query prover created");
     }
 
     if config.worker.instance_type >= WorkerClass::Medium {
         info!("Creating preprocessing prover");
-        register_v0_preprocessor(config, router);
+        register_v0_preprocessor(config, manager);
         info!("Preprocessing prover created");
     }
 
     if config.worker.instance_type >= WorkerClass::Large {
         info!("Creating groth16 prover");
-        register_v0_groth16_prover(config, router);
+        register_v0_groth16_prover(config, manager);
         info!("Groth16 prover created");
     }
 }
 
-fn register_v0_groth16_prover(config: &Config, router: &mut ProversManager) {
+fn register_v0_groth16_prover(config: &Config, manager: &mut ProversManager) {
     let params_config = &config.public_params;
     let assets = &params_config.groth16_assets;
     let groth16_prover = groth16::create_prover(
@@ -280,10 +280,10 @@ fn register_v0_groth16_prover(config: &Config, router: &mut ProversManager) {
     )
     .expect("Failed to create groth16 handler");
 
-    router.add_prover(ProverType::Query2Groth16, Box::new(groth16_prover));
+    manager.add_prover(ProverType::Query2Groth16, Box::new(groth16_prover));
 }
 
-fn register_v0_preprocessor(config: &Config, router: &mut ProversManager) {
+fn register_v0_preprocessor(config: &Config, manager: &mut ProversManager) {
     let params_config = &config.public_params;
     let preprocessing_prover = preprocessing::create_prover(
         &params_config.url,
@@ -295,10 +295,10 @@ fn register_v0_preprocessor(config: &Config, router: &mut ProversManager) {
     )
     .expect("Failed to create preprocessing handler");
 
-    router.add_prover(ProverType::Query2Preprocess, Box::new(preprocessing_prover));
+    manager.add_prover(ProverType::Query2Preprocess, Box::new(preprocessing_prover));
 }
 
-fn register_v0_ecr721_query_prover(config: &Config, router: &mut ProversManager) {
+fn register_v0_ecr721_query_prover(config: &Config, manager: &mut ProversManager) {
     let params_config = &config.public_params;
     let query2_prover = query::erc721::create_prover(
         &params_config.url,
@@ -310,10 +310,10 @@ fn register_v0_ecr721_query_prover(config: &Config, router: &mut ProversManager)
     )
     .expect("Failed to create query handler");
 
-    router.add_prover(ProverType::Query2Query, Box::new(query2_prover));
+    manager.add_prover(ProverType::Query2Query, Box::new(query2_prover));
 }
 
-fn register_v0_ecr20_query_prover(config: &Config, router: &mut ProversManager) {
+fn register_v0_ecr20_query_prover(config: &Config, manager: &mut ProversManager) {
     let params_config = &config.public_params;
     let query3_prover = query::erc20::create_prover(
         &params_config.url,
@@ -325,7 +325,7 @@ fn register_v0_ecr20_query_prover(config: &Config, router: &mut ProversManager) 
     )
     .expect("Failed to create query handler");
 
-    router.add_prover(ProverType::QueryErc20, Box::new(query3_prover));
+    manager.add_prover(ProverType::QueryErc20, Box::new(query3_prover));
 }
 
 fn verify_directory_checksums(
