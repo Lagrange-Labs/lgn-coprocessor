33245eaca452f654c996c9c00ab6ef9f39284881
delehef <franklin.delehelle@odena.eu>
2024-10-02 17:25:11 +0300

    refactor: only use combined routing key (#84)

--- lgn-messages/src/routing/mod.rs
+++ lgn-messages/src/routing/mod.rs
@@ -2,38 +2,22 @@
 use serde_derive::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash, Deserialize, Serialize)]
-pub enum RoutingKey {
-    /// Using integers for priority, for example: p1, p2, p3... p10
-    /// WARNING: Check [`PRIORITIES_LIMIT`]
-    Priority(u8),
-
-    /// Can specify domain, something like "sq" (storage query)
-    Domain(String),
-
-    /// Can specify both domain and priority
-    /// WARNING: Check [`PRIORITIES_LIMIT`]
-    Combined(String, u8),
+pub struct RoutingKey {
+    domain: String,
+    priority: u64,
 }
 
 impl RoutingKey {
-    pub fn priority(p: u8) -> anyhow::Result<Self> {
-        Ok(RoutingKey::Priority(p))
+    pub fn combined(domain: String, priority: u64) -> Self {
+        RoutingKey { domain, priority }
     }
 
-    pub fn domain(domain: String) -> anyhow::Result<Self> {
-        Ok(RoutingKey::Domain(domain))
-    }
-
-    pub fn combined(domain: String, priority: u8) -> anyhow::Result<Self> {
-        Ok(RoutingKey::Combined(domain, priority))
+    pub fn priority(&self) -> u64 {
+        self.priority
     }
 
     /// Get the route string for this routing key.
-    pub fn get_route(&self) -> anyhow::Result<String> {
-        match self {
-            RoutingKey::Priority(p) => Ok(format!("p{p}")),
-            RoutingKey::Domain(d) => Ok(d.clone()),
-            RoutingKey::Combined(domain, priority) => Ok(format!("{domain}_p{priority:?}")),
-        }
+    pub fn get_route(&self) -> String {
+        self.domain.clone()
     }
 }
