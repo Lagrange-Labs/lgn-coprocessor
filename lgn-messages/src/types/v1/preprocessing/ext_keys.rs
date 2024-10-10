c6b76cabc3e3df2b8f2e35f5e10e6b02a8474427
Augusto Hack <hack.augusto@gmail.com>
2024-10-11 01:54:32 +0200

    Revert "Final extraction merge message (#88)" (#89)
    This reverts commit 346ee4f68adeadf18bf33936a9c8059d818901cf.

--- lgn-messages/src/types/v1/preprocessing/ext_keys.rs
+++ lgn-messages/src/types/v1/preprocessing/ext_keys.rs
@@ -1,4 +1,3 @@
-use crate::types::v0::preprocessing::keys::{BlockNr, TableHash, TableId};
 use crate::types::v1::preprocessing::ext_tasks::MptNodeVersion;
 use crate::types::v1::preprocessing::KEYS_PREPROCESSING_PREFIX;
 use alloy_primitives::Address;
@@ -6,10 +5,17 @@ use object_store::path::Path;
 use serde_derive::{Deserialize, Serialize};
 use std::fmt::{Display, Formatter};
 
-const BLOCK_PREFIX: &str = "EXT_BLOCK";
-const CONTRACT_PREFIX: &str = "CONTRACT";
-const MPT_LENGTH_PREFIX: &str = "MPT_LENGTH";
+type TableId = u64;
+
+type BlockNr = u64;
+
 const MPT_VARIABLE_PREFIX: &str = "MPT_VARIABLE";
+
+const MPT_LENGTH_PREFIX: &str = "MPT_LENGTH";
+
+const CONTRACT_PREFIX: &str = "CONTRACT";
+
+const BLOCK_PREFIX: &str = "EXT_BLOCK";
 
 const FINAL_EXTRACTION_PREFIX: &str = "FINAL_EXTRACTION";
 #[derive(Debug, Clone, PartialEq, PartialOrd, Ord, Eq, Hash, Serialize, Deserialize)]
@@ -18,28 +24,19 @@ pub enum ProofKey {
     PublicParams,
 
     /// Indicates the location of `MPT` proof tree node.
-    MptVariable {
-        table_hash: TableHash,
-        mpt_node_version: MptNodeVersion,
-    },
+    MptVariable(TableId, MptNodeVersion),
 
     /// Indicates the location of Length slot proof.
-    MptLength {
-        table_hash: TableHash,
-        block_nr: BlockNr,
-    },
+    MptLength(TableId, BlockNr),
 
     /// Indicates the location of Contract proof.
-    Contract { address: Address, block_nr: BlockNr },
+    Contract(Address, BlockNr),
 
     /// Indicates the location of Block proof.
-    Block { block_nr: BlockNr },
+    Block(BlockNr),
 
     /// Indicates the location of FinalExtraction proof.
-    FinalExtraction {
-        table_id: TableId,
-        block_nr: BlockNr,
-    },
+    FinalExtraction(TableId, BlockNr),
 }
 
 impl Display for ProofKey {
@@ -48,33 +45,27 @@ impl Display for ProofKey {
             ProofKey::PublicParams => {
                 write!(f, "PublicParams_v1")
             }
-            ProofKey::MptVariable {
-                table_hash,
-                mpt_node_version,
-            } => {
+            ProofKey::MptVariable(table_id, node_version) => {
                 // Example: V1_PREPROCESSING/1/MPT_VARIABLE/1/0x1234_1
                 write!(
                     f,
                     "{}/{}/{}/{}/{}",
                     KEYS_PREPROCESSING_PREFIX,
-                    table_hash,
+                    table_id,
                     MPT_VARIABLE_PREFIX,
-                    mpt_node_version.0,
-                    mpt_node_version.1
+                    node_version.0,
+                    node_version.1
                 )
             }
-            ProofKey::MptLength {
-                table_hash,
-                block_nr,
-            } => {
+            ProofKey::MptLength(table_id, block_nr) => {
                 // Example: V1_PREPROCESSING/1/MPT_LENGTH/1
                 write!(
                     f,
                     "{}/{}/{}/{}",
-                    KEYS_PREPROCESSING_PREFIX, table_hash, MPT_LENGTH_PREFIX, block_nr
+                    KEYS_PREPROCESSING_PREFIX, table_id, MPT_LENGTH_PREFIX, block_nr
                 )
             }
-            ProofKey::Contract { address, block_nr } => {
+            ProofKey::Contract(address, block_nr) => {
                 // Example: V1_PREPROCESSING/CONTRACT/0x1234/1
                 write!(
                     f,
@@ -82,7 +73,7 @@ impl Display for ProofKey {
                     KEYS_PREPROCESSING_PREFIX, CONTRACT_PREFIX, address, block_nr
                 )
             }
-            ProofKey::Block { block_nr } => {
+            ProofKey::Block(block_nr) => {
                 // Example: V1_PREPROCESSING/EXT_BLOCK/1
                 write!(
                     f,
@@ -90,7 +81,7 @@ impl Display for ProofKey {
                     KEYS_PREPROCESSING_PREFIX, BLOCK_PREFIX, block_nr
                 )
             }
-            ProofKey::FinalExtraction { table_id, block_nr } => {
+            ProofKey::FinalExtraction(table_id, block_nr) => {
                 // Example: V1_PREPROCESSING/1/FINAL_EXTRACTION/1
                 write!(
                     f,
