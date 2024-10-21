a357c2cc7b9d2f855bc4bfb2b9fbb2a240309857
Fedor Sakharov <fedor.sakharov@gmail.com>
2024-10-21 12:24:35 +0200

    removes legacy fields from code

--- lgn-worker/src/main.rs
+++ lgn-worker/src/main.rs
@@ -297,8 +297,6 @@ async fn process_message_from_gateway(
                     let request = WorkerToGwRequest {
                         request: Some(lagrange::worker_to_gw_request::Request::WorkerDone(
                             WorkerDone {
-                                query_id: String::new(),
-                                task_id: String::new(),
                                 reply: Some(Reply::ReplyString(serde_json::to_string(&reply)?)),
                             },
                         )),
