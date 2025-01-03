d08498801f368ad06d900b340b0221469264a6b5
Steven Gu <asongala@163.com>
2025-01-03 17:58:26 +0800

    Update constants.

--- lgn-messages/src/types/v1/query/mod.rs
+++ lgn-messages/src/types/v1/query/mod.rs
@@ -14,13 +14,12 @@ pub mod tasks;
 
 pub const ROUTING_DOMAIN: &str = "sc";
 
-// TODO: need to update these two constants after benchmark.
 /// Maximum number of chunks that can be aggregated in a single proof of batching query
 /// We must use the same value of this constant for both DQ and Worker.
-pub const NUM_CHUNKS: usize = 5;
+pub const NUM_CHUNKS: usize = 66;
 /// Maximum number of rows that can be proven in a single proof of batching query
 /// We must use the same value of this constant for both DQ and Worker.
-pub const NUM_ROWS: usize = 5;
+pub const NUM_ROWS: usize = 25;
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct WorkerTask
--- lgn-provers/src/provers/v1/query/mod.rs
+++ lgn-provers/src/provers/v1/query/mod.rs
@@ -12,8 +12,8 @@ pub(crate) mod dummy_prover;
 #[cfg(not(feature = "dummy-prover"))]
 pub(crate) mod euclid_prover;
 
-pub const ROW_TREE_MAX_DEPTH: usize = 24;
-pub const INDEX_TREE_MAX_DEPTH: usize = 24;
+pub const ROW_TREE_MAX_DEPTH: usize = 25;
+pub const INDEX_TREE_MAX_DEPTH: usize = 26;
 pub const MAX_NUM_RESULT_OPS: usize = 20;
 pub const MAX_NUM_RESULTS: usize = 10;
 pub const MAX_NUM_OUTPUTS: usize = 3;
@@ -21,6 +21,7 @@ pub const MAX_NUM_ITEMS_PER_OUTPUT: usize = 5;
 pub const MAX_NUM_PLACEHOLDERS: usize = 10;
 pub const MAX_NUM_COLUMNS: usize = 20;
 pub const MAX_NUM_PREDICATE_OPS: usize = 20;
+
 #[allow(unused_variables)]
 pub fn create_prover(
     url: &str,
