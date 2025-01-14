e49533d36fac68ec3f2481af99846e89c771a55b
Steven Gu <asongala@163.com>
2025-01-14 17:59:53 +0800

    Add comments for prover trait functions.

--- lgn-provers/src/provers/v1/query/prover.rs
+++ lgn-provers/src/provers/v1/query/prover.rs
@@ -8,29 +8,34 @@ use verifiable_db::revelation::api::MatchingRow;
 
 pub trait StorageQueryProver
 {
+    /// Generate an universal circuit proof of a tabular query.
     fn prove_universal_circuit(
         &self,
         input: MatchingRowInput,
         pis: &DynamicCircuitPis,
     ) -> anyhow::Result<Vec<u8>>;
 
+    /// Generate a rows chunks proof of an aggregation (batching) query.
     fn prove_row_chunks(
         &self,
         input: RowsChunkInput,
         pis: &DynamicCircuitPis,
     ) -> anyhow::Result<Vec<u8>>;
 
+    /// Generate a chunk aggregation proof of an aggregation query.
     fn prove_chunk_aggregation(
         &self,
         chunks_proofs: &[Vec<u8>],
     ) -> anyhow::Result<Vec<u8>>;
 
+    /// Generate the proof for no rows satisfying the primary index query range.
     fn prove_non_existence(
         &self,
         input: NonExistenceInput,
         pis: &DynamicCircuitPis,
     ) -> anyhow::Result<Vec<u8>>;
 
+    /// Generate a revelation proof for an aggregation query.
     fn prove_aggregated_revelation(
         &self,
         pis: &DynamicCircuitPis,
@@ -39,6 +44,7 @@ pub trait StorageQueryProver
         indexing_proof: Vec<u8>,
     ) -> anyhow::Result<Vec<u8>>;
 
+    /// Generate a revelation proof for a tabular query.
     #[allow(clippy::too_many_arguments)]
     fn prove_tabular_revelation(
         &self,
