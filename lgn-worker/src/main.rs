2b49733da807599264c150a59573dfe9fb145267
Franklin Delehelle <franklin.delehelle@odena.eu>
2025-02-10 17:35:51 +0100

    warn -> trace

--- lgn-worker/src/main.rs
+++ lgn-worker/src/main.rs
@@ -261,7 +261,7 @@ async fn run_worker(
             Some(inbound_message) = inbound.next() => {
                 let msg = match inbound_message {
                     Ok(ref msg) => {
-                        tracing::warn!("inbound message: {:.2}KB", msg.task.len() as f32/1024.0);
+                        tracing::trace!("inbound message: {:.2}KB", msg.task.len() as f32/1024.0);
 
                         msg
                     },
@@ -363,7 +363,7 @@ async fn process_message_from_gateway(
     let outbound_msg = match reply {
         Ok(reply) => {
             let reply = serde_json::to_vec(&reply)?;
-            tracing::warn!("outbound message: {:.2}KB", reply.len() as f32 / 1024.0);
+            tracing::trace!("outbound message: {:.2}KB", reply.len() as f32 / 1024.0);
             WorkerToGwRequest {
                 request: Some(lagrange::worker_to_gw_request::Request::WorkerDone(
                     WorkerDone {
@@ -374,7 +374,7 @@ async fn process_message_from_gateway(
             }
         },
         Err(error_str) => {
-            tracing::warn!(
+            tracing::trace!(
                 "outbound message: {:.2}KB",
                 error_str.as_bytes().len() as f32 / 1024.0
             );
