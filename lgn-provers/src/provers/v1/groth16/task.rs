7797010d07e1e54b5067f5f7cf58581b4d2fac95
Augusto F. Hack <hack.augusto@gmail.com>
2025-03-05 12:38:49 +0100

    chore: removed single use methods

--- lgn-provers/src/provers/v1/groth16/task.rs
+++ lgn-provers/src/provers/v1/groth16/task.rs
@@ -3,7 +3,6 @@ use std::time::Instant;
 use anyhow::bail;
 use anyhow::Context;
 use lgn_messages::types::v1::groth16::keys::ProofKey;
-use lgn_messages::types::v1::groth16::WorkerTask;
 use lgn_messages::types::MessageEnvelope;
 use lgn_messages::types::MessageReplyEnvelope;
 use lgn_messages::types::ProofCategory;
@@ -21,7 +20,43 @@ impl<GP: Prover> LgnProver<TaskType, ReplyType> for Groth16<GP> {
         &self,
         envelope: &MessageEnvelope<TaskType>,
     ) -> anyhow::Result<MessageReplyEnvelope<ReplyType>> {
-        self.run_inner(envelope)
+        if let TaskType::V1Groth16(task) = envelope.inner() {
+            let now = Instant::now();
+            let key = ProofKey(envelope.query_id.to_string()).to_string();
+            let proof = self
+                .prover
+                .prove(task.revelation_proof.proof().as_slice())
+                .with_context(|| {
+                    format!(
+                        "Failed to generate the Groth16 proof: query_id = {}, task_id = {}",
+                        envelope.query_id, envelope.task_id,
+                    )
+                })?;
+            debug!(
+                "Finish generating the Groth16 proof: query_id = {}, task_id = {}",
+                envelope.query_id, envelope.task_id,
+            );
+
+            info!(
+                time = now.elapsed().as_secs_f32(),
+                proof_type = "groth16",
+                "proof generation time: {:?}",
+                now.elapsed()
+            );
+
+            let proof = (key, proof);
+            let reply = WorkerReply::new(task.chain_id, Some(proof), ProofCategory::Querying);
+
+            let reply_type = ReplyType::V1Groth16(reply);
+            let reply_envelope = MessageReplyEnvelope::new(
+                envelope.query_id.clone(),
+                envelope.task_id.clone(),
+                reply_type,
+            );
+            Ok(reply_envelope)
+        } else {
+            bail!("Unexpected task type: {:?}", envelope.inner());
+        }
     }
 }
 
@@ -33,66 +68,5 @@ pub struct Groth16<GP> {
 impl<GP: Prover> Groth16<GP> {
     pub(crate) fn new(prover: GP) -> Groth16<GP> {
         Self { prover }
-    }
-
-    pub(crate) fn run_inner(
-        &self,
-        envelope: &MessageEnvelope<TaskType>,
-    ) -> anyhow::Result<MessageReplyEnvelope<ReplyType>> {
-        let query_id = envelope.query_id.clone();
-        let task_id = envelope.task_id.clone();
-        if let TaskType::V1Groth16(task) = envelope.inner() {
-            let reply = self.process_task(query_id.clone(), task_id.clone(), task)?;
-            let reply_type = ReplyType::V1Groth16(reply);
-            let reply_envelope = MessageReplyEnvelope::new(query_id, task_id, reply_type);
-            Ok(reply_envelope)
-        } else {
-            bail!("Unexpected task type: {:?}", envelope.inner());
-        }
-    }
-
-    fn process_task(
-        &self,
-        query_id: String,
-        task_id: String,
-        task: &WorkerTask,
-    ) -> anyhow::Result<WorkerReply> {
-        let proof = self.generate_proof(
-            &query_id,
-            &task_id,
-            task.revelation_proof.proof().as_slice(),
-        )?;
-        Ok(WorkerReply::new(
-            task.chain_id,
-            Some(proof),
-            ProofCategory::Querying,
-        ))
-    }
-
-    /// Generate the Groth proof.
-    fn generate_proof(
-        &self,
-        query_id: &str,
-        task_id: &str,
-        revelation: &[u8],
-    ) -> anyhow::Result<(String, Vec<u8>)> {
-        // Generate the Groth16 proof.
-        let now = Instant::now();
-        let key = ProofKey(query_id.to_string()).to_string();
-        let proof = self.prover.prove(revelation).with_context(|| {
-            format!(
-                " Failed to generate the Groth16 proof: query_id = {query_id}, task_id = {task_id}"
-            )
-        })?;
-        debug!("Finish generating the Groth16 proof: query_id = {query_id}, task_id = {task_id}",);
-
-        info!(
-            time = now.elapsed().as_secs_f32(),
-            proof_type = "groth16",
-            "proof generation time: {:?}",
-            now.elapsed()
-        );
-
-        Ok((key, proof))
     }
 }
