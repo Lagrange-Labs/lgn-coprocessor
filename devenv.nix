887e4a55189663011de5244744cb1edcaa447d35
Franklin Delehelle <franklin.delehelle@odena.eu>
2025-04-07 12:49:08 +0200

    build: simplify worker configuration in devenv

--- devenv.nix
+++ devenv.nix
@@ -2,7 +2,6 @@
 
 let
   meta = {
-    version = "develop";
     keystore-file = "./runtime/lagr-keystore.json";
     keystore-password = "canihazsecurityplz";
     gateway-url = "http://localhost:10000";
@@ -11,43 +10,24 @@ let
 
   avsWorkerConfig = {
     worker = {
-      version = meta.version;
       instance_type = "medium";
     };
 
     avs = {
       gateway_url = meta.gateway-url;
-      issuer = "Some AVS partner";
-      worker_id = "worker_id";
       lagr_keystore = meta.keystore-file;
       lagr_pwd = meta.keystore-password;
     };
 
-    prometheus.port = 9090;
-
     public_params = {
       params_root_url = meta.params-url;
       dir = "./runtime/zkmr_params";
-
-      preprocessing_params = {
-        file = "preprocessing_params.bin";
-      };
-      query_params = {
-        file = "query_params.bin";
-      };
-      groth16_assets = {
-        circuit_file = "groth16_assets/circuit.bin";
-        r1cs_file = "groth16_assets/r1cs.bin";
-        pk_file = "groth16_assets/pk.bin";
-      };
     };
   };
 
   lagrangeWorkerConfig = avsWorkerConfig //
                          { avs = {
                              gateway_url = meta.gateway-url;
-                             issuer = "Lagrange";
-                             worker_id = "lagrange-medium";
                              lagr_private_key = config.env.LAGRANGE_PRIVATE_KEY;
                            }; };
 
