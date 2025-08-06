eca1d135a69b77bd17e60ac5d45afd3de4668311
Augusto Hack <hack.augusto@gmail.com>
2025-08-06 16:05:32 +0200

    refactor: mmap query and preprocessing PPs (#237)
    * feat: mmap query and preprocessing param files
    
    * fix: inverted logic

--- lgn-provers/src/params/mod.rs
+++ lgn-provers/src/params/mod.rs
@@ -1,5 +1,4 @@
 use std::collections::HashMap;
-use std::path::Path;
 use std::path::PathBuf;
 use std::str::FromStr;
 use std::time::Duration;
@@ -7,12 +6,10 @@ use std::time::Duration;
 use anyhow::bail;
 use anyhow::ensure;
 use anyhow::Context;
-use bytes::Bytes;
 use futures::StreamExt;
 use reqwest::StatusCode;
 use tokio::fs::File;
 use tokio::io::AsyncWriteExt;
-use tracing::error;
 use tracing::info;
 use tracing::warn;
 
@@ -39,7 +36,6 @@ const DOWNLOAD_BACKOFF_MIN_MILLIS: u64 = 100;
 const DOWNLOAD_BACKOFF_MAX_MILLIS: u64 = 10_000;
 
 /// Ratio to convert byte to megabytes.
-const BYTES_TO_MEGABYTES_USIZE: usize = 1024 * 1024;
 const BYTES_TO_MEGABYTES_U64: u64 = 1024 * 1024;
 
 /// Download and verify `file_name`.
@@ -55,7 +51,7 @@ pub async fn download_and_checksum(
     param_dir: &str,
     file_name: &str,
     checksums: &HashMap<String, blake3::Hash>,
-) -> anyhow::Result<Bytes> {
+) -> anyhow::Result<PathBuf> {
     let mut filepath = PathBuf::from(param_dir);
     filepath.push(file_name);
 
@@ -84,16 +80,14 @@ pub async fn download_and_checksum(
         .with_context(|| format!("Failed to create file. filepath: {}", filepath.display()))?;
 
     let mut hasher = blake3::Hasher::new();
-    let mut buf = std::fs::read(&filepath)
-        .with_context(|| format!("Reading file failed. filepath: {}", filepath.display()))?;
-    hasher.update_rayon(&buf);
+    hasher.update_mmap_rayon(&filepath)?;
 
     if *expected_checksum == hasher.finalize() {
         info!(
             "Found file matching checksum, skipping download. filepath: {}",
             filepath.display()
         );
-        return Ok(Bytes::from(buf));
+        return Ok(filepath);
     }
 
     let fileurl = format!("{base_url}/{file_name}");
@@ -111,10 +105,7 @@ pub async fn download_and_checksum(
 
     for (retry, duration) in backoff.iter().enumerate() {
         let result = resume_download(
-            &mut buf,
-            base_url,
             &mut file,
-            &filepath,
             &fileurl,
             retry,
             &client,
@@ -125,12 +116,8 @@ pub async fn download_and_checksum(
 
         match result {
             Ok(()) => {
-                info!(
-                    "Params loaded. filepath: {} size: {}MiB",
-                    filepath.display(),
-                    buf.len() / BYTES_TO_MEGABYTES_USIZE,
-                );
-                return Ok(Bytes::from(buf));
+                info!("Downloaded file. filepath: {}", filepath.display());
+                return Ok(filepath);
             },
             Err(err) => {
                 if let Some(duration) = duration {
@@ -154,10 +141,7 @@ pub async fn download_and_checksum(
 
 #[allow(clippy::too_many_arguments)]
 async fn resume_download(
-    buf: &mut Vec<u8>,
-    base_url: &str,
     file: &mut File,
-    filepath: &Path,
     fileurl: &str,
     retry: usize,
     client: &reqwest::Client,
@@ -182,66 +166,44 @@ async fn resume_download(
                 Ok(parsed)
             })?;
 
+    if response.status() == StatusCode::RANGE_NOT_SATISFIABLE {
+        hasher.reset();
+        file.set_len(0).await?;
+        bail!("Local file is bigger than remote, reset file and restart download");
+    }
+
+    ensure!(
+        response.status().is_success(),
+        "Requesting params failed. status: {}",
+        response.status(),
+    );
+
     info!(
-        "Downloading params. base_url: {} filepath: {} present: {}MiB download: {}MiB retry: {}",
-        base_url,
-        filepath.display(),
+        "Downloading params. fileurl: {} present: {}MiB download: {}MiB retry: {}",
+        fileurl,
         metadata.len() / BYTES_TO_MEGABYTES_U64,
         length / BYTES_TO_MEGABYTES_U64,
         retry,
     );
 
-    if response.status() == StatusCode::RANGE_NOT_SATISFIABLE {
-        warn!(
-            "Local file is bigger than remote, resetting length and checking checksum. filepath: {}",
-            filepath.display(),
-        );
-
-        hasher.reset();
-        buf.resize(
-            length.try_into().expect("File size should fit in a usize"),
-            0,
-        );
-        file.set_len(length).await?;
-        hasher.update_rayon(buf);
-    } else {
-        ensure!(
-            response.status().is_success(),
-            "Requesting params failed. status: {} filepath: {}",
-            response.status(),
-            filepath.display(),
-        );
-
-        let mut stream = response.bytes_stream();
-        while let Some(data) = stream.next().await {
-            let data = data?;
-            file.write_all(&data).await?;
-            hasher.update_rayon(&data);
-            buf.extend(data);
-        }
+    let mut stream = response.bytes_stream();
+    while let Some(data) = stream.next().await {
+        let data = data?;
+        file.write_all(&data).await?;
+        hasher.update_rayon(&data);
     }
 
     let found_checksum = hasher.finalize();
     if found_checksum != *expected_checksum {
-        error!(
-            "Checksum failed, restarting download. checksum: {} expected: {} filepath: {}",
-            found_checksum.to_hex(),
-            expected_checksum.to_hex(),
-            filepath.display(),
-        );
-
         hasher.reset();
-        buf.clear();
         file.set_len(0).await?;
 
         bail!(
-            "Checksum failed, restarting download. checksum: {} expected: {} filepath: {}",
+            "Checksum failed, restarting download. checksum: {} expected: {}",
             found_checksum.to_hex(),
             expected_checksum.to_hex(),
-            filepath.display(),
         );
     } else {
-        info!("Downloaded file. filepath: {}", filepath.display());
         Ok(())
     }
 }
